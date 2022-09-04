b51c353c281ed6860d4c302fcbb3b5d12dcb9473
Nadine Doranco <havanueva@gmail.com>
2022-09-04 12:48:51 +0200

    connection-firt

--- src/main/java/com/appli/springbootsecurityjpa/SpringBootSecurityJpaApplication.java
+++ src/main/java/com/appli/springbootsecurityjpa/SpringBootSecurityJpaApplication.java
@@ -2,12 +2,12 @@ package com.appli.springbootsecurityjpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class SpringBootSecurityJpaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSecurityJpaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityJpaApplication.class, args);
+    }
 }
